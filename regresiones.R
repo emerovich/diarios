c73154f776cc499bedbe1d0bdbc6750ccdbde0b2
emerovich <53981470+emerovich@users.noreply.github.com>
2020-06-25 17:34:53 -0300

    Incorporé modelos y output con stargazer

--- regresiones.R
+++ regresiones.R
@@ -1,12 +1,14 @@
 #Tengo que haber corrido procesa_texto_clarin para correr este programa
 #Las series son inflacion_argentina, test_inflacion y sentimiento
+library(car)
+library(stargazer)
 
 indice_atencion <- test_inflacion
 sentimiento <- output
 
 inflacion <- inflacion_argentina
 inflacion$inflacion_mensual <- (inflacion$inflacion_mensual-mean(inflacion$inflacion_mensual))/sd(inflacion$inflacion_mensual)
-inflacion <- inflacion[1:(nrow(inflacion)-1),] #esto lo hago porque para la inflacion del periodo t quiero tomar el indice de atencion del periodo t. Antes tenia un mes más para la inflacion
+inflacion <- inflacion[1:(nrow(inflacion)-1),] #esto lo hago porque para la inflacion del periodo t quiero tomar el indice de atencion del periodo t. Antes tenia un mes mÃ¡s para la inflacion
 
 #Chequeo que las tres series tengan la misma extension
 nrow(inflacion)
@@ -38,6 +40,7 @@ inflacion_tres_rezagos <- inflacion[(1:(nrow(inflacion)-3)),2]
 
 
 inflacion_var_explicada <- inflacion[4:nrow(inflacion),2]
+indice_atencion_var_explicada <- indice_atencion[4:nrow(indice_atencion),2]
 
 #Chequeo que tenga el length adecuado
 length(indice_atencion_un_rezago)
@@ -64,12 +67,20 @@ inflacion_un_rezago <- as.vector(unlist(inflacion_un_rezago))
 inflacion_dos_rezagos <- as.vector(unlist(inflacion_dos_rezagos))
 inflacion_tres_rezagos <- as.vector(unlist(inflacion_tres_rezagos))
 
+
+### MODELO CON INFLACION COMO VARIABLE EXPLICADA
+
 modelo <- lm(inflacion_var_explicada ~ inflacion_un_rezago + inflacion_dos_rezagos +
      inflacion_tres_rezagos + indice_atencion_un_rezago + indice_atencion_dos_rezagos + 
      indice_atencion_tres_rezagos + sentimiento_un_rezago + sentimiento_dos_rezagos + 
      sentimiento_tres_rezagos)
 
 summary(modelo)
+
+linearHypothesis(modelo, c("inflacion_un_rezago", "inflacion_dos_rezagos", "inflacion_tres_rezagos",
+                           "indice_atencion_un_rezago", "indice_atencion_dos_rezagos", "indice_atencion_tres_rezagos",
+                           "sentimiento_un_rezago", "sentimiento_dos_rezagos",
+                           "sentimiento_tres_rezagos"))
 
 modelo2 <- lm(inflacion_var_explicada ~ inflacion_un_rezago + inflacion_dos_rezagos +
                inflacion_tres_rezagos + indice_atencion_un_rezago + indice_atencion_dos_rezagos + 
@@ -82,3 +93,30 @@ modelo3 <- lm(inflacion_var_explicada ~ inflacion_un_rezago + inflacion_dos_reza
 
 summary(modelo3)
 
+stargazer(list(modelo, modelo2, modelo3), type="text", keep.stat = c("n","rsq", "adj.rsq", "f"), out = "tabla1.htm")
+
+### MODELO CON INDICE DE ATENCION COMO VARIABLE EXPLICADA
+
+indice_atencion_un_rezago <- indice_atencion[(3:(nrow(indice_atencion)-1)),2]
+indice_atencion_dos_rezagos <- indice_atencion[(2:(nrow(indice_atencion)-2)),2]
+indice_atencion_tres_rezagos <- indice_atencion[(1:(nrow(indice_atencion)-3)),2]
+
+indice_atencion_un_rezago <- indice_atencion_un_rezago
+indice_atencion_dos_rezagos <- indice_atencion_dos_rezagos
+indice_atencion_tres_rezagos <- indice_atencion_tres_rezagos
+
+modelo4 <- lm(indice_atencion_var_explicada ~ inflacion_un_rezago + inflacion_dos_rezagos +
+               inflacion_tres_rezagos + indice_atencion_un_rezago + indice_atencion_dos_rezagos + 
+               indice_atencion_tres_rezagos + sentimiento_un_rezago + sentimiento_dos_rezagos + 
+               sentimiento_tres_rezagos)
+
+summary(modelo4)
+
+modelo5 <- lm(indice_atencion_var_explicada ~ inflacion_un_rezago + inflacion_dos_rezagos +
+                inflacion_tres_rezagos + indice_atencion_un_rezago + indice_atencion_dos_rezagos + 
+                indice_atencion_tres_rezagos)
+
+modelo6 <- lm(indice_atencion_var_explicada ~ indice_atencion_un_rezago + indice_atencion_dos_rezagos + 
+                indice_atencion_tres_rezagos)
+
+stargazer(list(modelo4, modelo5, modelo6), type="text", keep.stat = c("n","rsq", "adj.rsq", "f"), out = "tabla2.htm")
